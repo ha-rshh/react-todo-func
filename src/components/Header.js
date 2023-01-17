b46b70ce5c8b28be5bb0ed8b65c3867f4ece9684
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-01-17 18:59:47 +0530

    added styling

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      <div className="navBar">
+        <h1>
+          Todo Hanger<span className="subscript">Your personal todo app</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
